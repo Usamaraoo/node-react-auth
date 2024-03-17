8e6b65d585b631de63b38cbb972c9b93e06e879d
UsamaKaleemTechmate <usamakaleem505@gmail.com>
2024-03-17 23:14:32 +0500

    redux presist added

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,13 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Tuple, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./redux/features/user/userSlice";
+import { FLUSH, persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([thunk]),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store)
